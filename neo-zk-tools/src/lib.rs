213b9959177b78c6815409d5bb38f54d4a5b1951
passer-byzhang <1541115709@qq.com>
2022-04-06 12:00:24 +0800

    delete test

--- neo-zk-tools/src/lib.rs
+++ neo-zk-tools/src/lib.rs
@@ -69,69 +69,6 @@ pub extern "C" fn gt_neg_mul() -> *const gtobject {
 }
 
 #[no_mangle]
-pub extern "C" fn gt_add_test() -> *const gtobject {
-    let gt1 = pairing(&G1Affine::generator(), &G2Affine::generator());
-    let gt = gt1 + gt1;
-    let result_bytes = gt_to_bytes(gt);
-    let resultobj = gtobject { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}
-
-/*#[no_mangle]
-pub extern "C" fn gt_mul_test() -> *const gtobject{
-    use bls12_381::Scalar;
-    let mut gt1 = pairing(&G1Affine::generator(), &G2Affine::generator());
-    let gt1 = gt1 * Scalar::from(3);
-    let result_bytes = gt_to_bytes(gt1);
-    let resultobj = gtobject { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}
-
-#[no_mangle]
-pub extern "C" fn g1_add_test() -> *const g1object{
-    let g1 =G1Projective::from(&G1Affine::generator());
-    let g1_result = G1Affine::from(g1+g1);
-    let result_bytes = g1_to_bytes(g1_result);
-    let resultobj = g1object { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}
-
-#[no_mangle]
-pub extern "C" fn g1_mul_test() -> *const g1object{
-    use bls12_381::Scalar;
-    let g1 =G1Affine::generator();
-    let g1_result = G1Affine::from(g1 * Scalar::from(3));
-    let result_bytes = g1_to_bytes(g1_result);
-    let resultobj = g1object { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}
-
-#[no_mangle]
-pub extern "C" fn g2_add_test() -> *const g2object{
-    let g2 =G2Projective::from(&G2Affine::generator());
-    let g2_result = G2Affine::from(g2+g2);
-    let result_bytes = g2_to_bytes(g2_result);
-    let resultobj = g2object { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}
-
-#[no_mangle]
-pub extern "C" fn g2_mul_test() -> *const g2object{
-    use bls12_381::Scalar;
-    let g2 =G2Affine::generator();
-    let g2_result = G2Affine::from(g2 * Scalar::from(3));
-    let result_bytes = g2_to_bytes(g2_result);
-    let resultobj = g2object { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}*/
-
-#[no_mangle]
 pub extern "C" fn g1_add(g1_1: *mut g1object, g1_2: *mut g1object) -> *const g1object {
     let g1_1_bytes = unsafe { g1_1.as_mut().expect("g1_add::invalid g1_1 ptr").val };
     let g1_2_bytes = unsafe { g1_2.as_mut().expect("g1_add::invalid g1_2 ptr").val };
@@ -204,13 +141,6 @@ pub extern "C" fn g1_dispose(ptr: *mut g1object) {
     }
 }
 
-/*
-#[no_mangle]
-pub extern "C" fn g1_check(ptr: *mut g1object) {
-    let obj = unsafe { ptr.as_mut().expect("invalid ptr") };
-    println!("传回rust的东西： {:?}", obj.val)
-}*/
-
 #[no_mangle]
 pub extern "C" fn g2_generator() -> *const g2object {
     let g2_bytes = g2_to_bytes(G2Affine::generator());
@@ -226,21 +156,6 @@ pub extern "C" fn g2_dispose(ptr: *mut g2object) {
         Box::from_raw(ptr);
     }
 }
-/*
-#[no_mangle]
-pub extern "C" fn g2_check(ptr: *mut g2object) {
-    let obj = unsafe { ptr.as_mut().expect("invalid ptr") };
-    println!("传回rust的东西： {:?}", obj.val)
-}
-
-#[no_mangle]
-pub extern "C" fn gt_identity() -> *const gtobject {
-    let gt_bytes = gt_to_bytes(Gt::identity());
-    let obj = gtobject { val: gt_bytes };
-    println!("rust传出的东西： {:?}", obj.val);
-    let b = Box::new(obj);
-    return Box::into_raw(b);
-}*/
 
 #[no_mangle]
 pub extern "C" fn gt_dispose(ptr: *mut gtobject) {
@@ -248,20 +163,4 @@ pub extern "C" fn gt_dispose(ptr: *mut gtobject) {
         Box::from_raw(ptr);
     }
 }
-/*
-#[no_mangle]
-pub extern "C" fn gt_check(ptr: *mut gtobject) {
-    let obj = unsafe { ptr.as_mut().expect("invalid ptr") };
-    println!("传回rust的东西： {:?}", obj.val)
-}
 
-#[no_mangle]
-pub extern "C" fn test_generator_pairing() -> *const gtobject{
-    let g1 = G1Affine::generator();
-    let g2 = G2Affine::generator();
-    let result = pairing(&g1, &g2);
-    let result_bytes = gt_to_bytes(result);
-    let resultobj = gtobject { val: result_bytes };
-    let b = Box::new(resultobj);
-    Box::into_raw(b)
-}*/
